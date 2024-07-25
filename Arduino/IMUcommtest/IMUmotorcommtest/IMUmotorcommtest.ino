ca216baf9c1685c4b2af6cbe1f3f5cf404054554
Neil Wagner <nrwagner29@gmail.com>
2024-07-25 08:17:42 -0500

    MPU reading code

--- Arduino/IMUcommtest/IMUmotorcommtest/IMUmotorcommtest.ino
+++ Arduino/IMUcommtest/IMUmotorcommtest/IMUmotorcommtest.ino
@@ -2,6 +2,95 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+//#include "MPU6050.h" // not necessary if using MotionApps include file
+
+// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
+// is used in I2Cdev.h
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+#include "Wire.h"
+#endif
+
+// class default I2C address is 0x68
+// specific I2C addresses may be passed as a parameter here
+// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
+// AD0 high = 0x69
+MPU6050 i2cmpu;
+//MPU6050 mpu(0x69); // <-- use for AD0 high
+
+// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
+// quaternion components in a [w, x, y, z] format (not best for parsing
+// on a remote host such as Processing or something though)
+//#define OUTPUT_READABLE_QUATERNION
+
+// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
+// (in degrees) calculated from the quaternions coming from the FIFO.
+// Note that Euler angles suffer from gimbal lock (for more info, see
+// http://en.wikipedia.org/wiki/Gimbal_lock)
+//#define OUTPUT_READABLE_EULER
+
+// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
+// pitch/roll angles (in degrees) calculated from the quaternions coming
+// from the FIFO. Note this also requires gravity vector calculations.
+// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
+// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
+// components with gravity removed. This acceleration reference frame is
+// not compensated for orientation, so +X is always +X according to the
+// sensor, just without the effects of gravity. If you want acceleration
+// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
+#define OUTPUT_READABLE_REALACCEL
+
+// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
+// components with gravity removed and adjusted for the world frame of
+// reference (yaw is relative to initial orientation, since no magnetometer
+// is present in this case). Could be quite handy in some cases.
+#define OUTPUT_READABLE_WORLDACCEL
+
+// uncomment "OUTPUT_TEAPOT" if you want output that matches the
+// format used for the InvenSense teapot demo
+//#define OUTPUT_TEAPOT
+
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 13       // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;   // set true if DMP init was successful
+uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
+uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;      // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64];  // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;         // [w, x, y, z]         quaternion container
+VectorInt16 aa;       // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;   // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;  // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;  // [x, y, z]            gravity vector
+float euler[3];       // [psi, theta, phi]    Euler angle container
+float ypr[3];         // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n' };
+
+
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;  // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
 
 
 
@@ -12,24 +101,38 @@ BrushlessDriveClient motl(2);
 PowerMonitorClient pwrr(0);
 PowerMonitorClient pwrl(2);
 int i = 0;
-int x = 0;
-int y = 0;
-float axo = -0.8;
-float ayo = 0.05;
-float azo = 1.83;
-float gxo = 0.06;
-float gyo = 0.01;
-float gzo = 0.02;
+float axo = 0;
+float ayo = 0;
+float azo = 0;
+float gxo = 0;
+float gyo = 0;
+float gzo = 0;
 
 void setup() {
   // Initialize serial and wait for port to open:
-  // Initialize the IqSerial object    
+  // Initialize the IqSerial object
   ser.begin();
 
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+
+
   Serial.begin(9600);
-  while (!Serial);  // will pause MKR until serial console opens
+  while (!Serial)
+    ;  // will pause MKR until serial console opens
+
+
   Serial.println("Adafruit MPU6050 and Vertiq Motor Test!");
+
+
   // Try to initialize!
+
+
   if (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     while (1) {
@@ -38,64 +141,65 @@ void setup() {
   }
   Serial.println("MPU6050 Found!");
 
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-    case MPU6050_RANGE_2_G:
-      Serial.println("+-2G");
-      break;
-    case MPU6050_RANGE_4_G:
-      Serial.println("+-4G");
-      break;
-    case MPU6050_RANGE_8_G:
-      Serial.println("+-8G");
-      break;
-    case MPU6050_RANGE_16_G:
-      Serial.println("+-16G");
-      break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-    case MPU6050_RANGE_250_DEG:
-      Serial.println("+- 250 deg/s");
-      break;
-    case MPU6050_RANGE_500_DEG:
-      Serial.println("+- 500 deg/s");
-      break;
-    case MPU6050_RANGE_1000_DEG:
-      Serial.println("+- 1000 deg/s");
-      break;
-    case MPU6050_RANGE_2000_DEG:
-      Serial.println("+- 2000 deg/s");
-      break;
-  }
+Serial.println(F("Initializing I2C devices..."));
+    i2cmpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
 
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-    case MPU6050_BAND_260_HZ:
-      Serial.println("260 Hz");
-      break;
-    case MPU6050_BAND_184_HZ:
-      Serial.println("184 Hz");
-      break;
-    case MPU6050_BAND_94_HZ:
-      Serial.println("94 Hz");
-      break;
-    case MPU6050_BAND_44_HZ:
-      Serial.println("44 Hz");
-      break;
-    case MPU6050_BAND_21_HZ:
-      Serial.println("21 Hz");
-      break;
-    case MPU6050_BAND_10_HZ:
-      Serial.println("10 Hz");
-      break;
-    case MPU6050_BAND_5_HZ:
-      Serial.println("5 Hz");
-      break;
-  }
+    // verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(i2cmpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = i2cmpu.dmpInitialize();
+
+    // supply your own gyro offsets here, scaled for min sensitivity
+    i2cmpu.setXGyroOffset(220);
+    i2cmpu.setYGyroOffset(76);
+    i2cmpu.setZGyroOffset(-85);
+    i2cmpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+ // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        i2cmpu.CalibrateAccel(6);
+        i2cmpu.CalibrateGyro(6);
+        i2cmpu.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        i2cmpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+        Serial.println(F(")..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = i2cmpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = i2cmpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
   Serial.println("");
 
   delay(100);
@@ -103,16 +207,103 @@ void setup() {
 
 void loop() {
   // Your code here
- 
+ // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+    // read a packet from FIFO
+    if (i2cmpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            i2cmpu.dmpGetQuaternion(&q, fifoBuffer);
+            Serial.print(" quat\t");
+            Serial.print(q.w);
+            Serial.print("\t");
+            Serial.print(q.x);
+            Serial.print("\t");
+            Serial.print(q.y);
+            Serial.print("\t");
+            Serial.print(q.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            i2cmpu.dmpGetQuaternion(&q, fifoBuffer);
+            i2cmpu.dmpGetEuler(euler, &q);
+            Serial.print(" euler\t");
+            Serial.print(euler[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(euler[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(euler[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            i2cmpu.dmpGetQuaternion(&q, fifoBuffer);
+            i2cmpu.dmpGetGravity(&gravity, &q);
+            i2cmpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print(" ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // display real acceleration, adjusted to remove gravity
+            i2cmpu.dmpGetQuaternion(&q, fifoBuffer);
+            i2cmpu.dmpGetAccel(&aa, fifoBuffer);
+            i2cmpu.dmpGetGravity(&gravity, &q);
+            i2cmpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            Serial.print(" areal\t");
+            Serial.print(aaReal.x);
+            Serial.print("\t");
+            Serial.print(aaReal.y);
+            Serial.print("\t");
+            Serial.print(aaReal.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            i2cmpu.dmpGetQuaternion(&q, fifoBuffer);
+            i2cmpu.dmpGetAccel(&aa, fifoBuffer);
+            i2cmpu.dmpGetGravity(&gravity, &q);
+            i2cmpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            i2cmpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print(" aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.print(aaWorld.z);
+        #endif
+    
+        #ifdef OUTPUT_TEAPOT
+            // display quaternion values in InvenSense Teapot demo format:
+            teapotPacket[2] = fifoBuffer[0];
+            teapotPacket[3] = fifoBuffer[1];
+            teapotPacket[4] = fifoBuffer[4];
+            teapotPacket[5] = fifoBuffer[5];
+            teapotPacket[6] = fifoBuffer[8];
+            teapotPacket[7] = fifoBuffer[9];
+            teapotPacket[8] = fifoBuffer[12];
+            teapotPacket[9] = fifoBuffer[13];
+            Serial.write(teapotPacket, 14);
+            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+        #endif
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+    }
   /* Get new sensor events with the readings */
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
 
   // Send voltage signal to motors
   static float velocityr = 0;
   static float velocityl = 0;
-  float Motorcommandr = a.acceleration.z + azo;
-  float Motorcommandl = a.acceleration.x + axo;
+  float Motorcommandr =  azo; //fix
+  float Motorcommandl =  axo;
   float voltager = 0;
   float voltagel = 0;
   //supply voltage
@@ -120,56 +311,34 @@ void loop() {
   ser.set(motl.drive_spin_volts_, Motorcommandl);
 
 
-    if (ser.get(pwrr.volts_, voltager)) {
-      Serial.print("Motor R Supply Voltage: ");
-      Serial.print(voltager);
-    }
-    if (ser.get(pwrl.volts_, voltagel)) {
-      Serial.print(" Motor L Supply Voltage: ");
-      Serial.print(voltagel);
-    }
-    // Get the velocity. If a response was received...
+  if (ser.get(pwrr.volts_, voltager)) {
+    Serial.print("Motor R Supply Voltage: ");
+    Serial.print(voltager);
+  }
+  if (ser.get(pwrl.volts_, voltagel)) {
+    Serial.print(" Motor L Supply Voltage: ");
+    Serial.print(voltagel);
+  }
+  // Get the velocity. If a response was received...
 
-    if (ser.get(motr.obs_velocity_, velocityr)) {
-      // Display the reported velocity
-      Serial.print(" Voltage R Command: ");
-      Serial.print(Motorcommandr);
-      Serial.print(" Motor R Velocity: ");
-      Serial.print(velocityr);
-    } else {
-      Serial.println("No Response Received");
-    }
+  if (ser.get(motr.obs_velocity_, velocityr)) {
+    // Display the reported velocity
+    Serial.print(" Voltage R Command: ");
+    Serial.print(Motorcommandr);
+    Serial.print(" Motor R Velocity: ");
+    Serial.print(velocityr);
+  } else {
+    Serial.print(" No Response Received ");
+  }
 
-    if (ser.get(motl.obs_velocity_, velocityl)) {
-      // Display the reported velocity
-      Serial.print(" Voltage L Command: ");
-      Serial.print(Motorcommandl);
-      Serial.print(" Motor L Velocity: ");
-      Serial.print(velocityl);
-    } else {
-      Serial.println("No Response Received");
-    }
-
-    /* Print out the values */
-    Serial.print(" Acceleration X: ");
-    Serial.print(a.acceleration.x + axo);
-    Serial.print(", Y: ");
-    Serial.print(a.acceleration.y + ayo);
-    Serial.print(", Z: ");
-    Serial.print(a.acceleration.z + azo);
-    Serial.print(" m/s^2 ");
-
-    Serial.print("Rotation X: ");
-    Serial.print(g.gyro.x + gxo);
-    Serial.print(", Y: ");
-    Serial.print(g.gyro.y + gyo);
-    Serial.print(", Z: ");
-    Serial.print(g.gyro.z + gzo);
-    Serial.print(" rad/s ");
-
-    Serial.print("Temperature: ");
-    Serial.print(temp.temperature);
-    Serial.println(" degC");
-   
-    
+  if (ser.get(motl.obs_velocity_, velocityl)) {
+    // Display the reported velocity
+    Serial.print(" Voltage L Command: ");
+    Serial.print(Motorcommandl);
+    Serial.print(" Motor L Velocity: ");
+    Serial.print(velocityl);
+  } else {
+    Serial.print(" No Response Received ");
+  }
+Serial.println(" ");
 }
